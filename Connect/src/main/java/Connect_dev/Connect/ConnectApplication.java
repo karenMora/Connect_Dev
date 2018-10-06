3af20ca07695979a71b23982a86af99d2725e834
Rafael Jimenez <rafael.jimenez@mail.escuelaing.edu.co>
2018-10-06 12:00:17 -0500

    Primer commit

---
+++ Connect/src/main/java/Connect_dev/Connect/ConnectApplication.java
@@ -0,0 +1,12 @@
+package Connect_dev.Connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectApplication.class, args);
+	}
+}
