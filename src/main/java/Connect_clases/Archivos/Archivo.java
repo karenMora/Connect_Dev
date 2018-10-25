02047fae5e2cb1f114aafac1a82aa76f1907c06e
Miguel Rojas <miguel.rojas-ma@mail.escuelaing.edu.co>
2018-10-24 20:46:55 -0500

    Clases Archivos

--- src/main/java/Connect_clases/Archivos/Archivo.java
+++ src/main/java/Connect_clases/Archivos/Archivo.java
@@ -3,12 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Connect_clases.Archivo;
+package Connect_clases.Archivos;
+
+import java.io.File;
 
 /**
  *
  * @author miguel
  */
 public class Archivo {
+    
+    File archivo;
+    
+    public Archivo(String nameFile){
+        archivo = new File(""+nameFile+".txt");
+    }
     
 }
